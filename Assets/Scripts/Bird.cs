27f6e8aae0bd9bd5f7e23d04529b9347e6a74c71
Your Name <you@example.com>
2018-12-04 08:16:52 +0700

    Add scene02

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+
+    Animator m_Animator;
+    public float aniSpeed;
+
+    void Start()
+    {
+        //Get the animator, attached to the GameObject you are intending to animate.
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    void OnGUI()
+    {
+
+        m_Animator.speed = aniSpeed;
+    }
+}
---
+++ Assets/Scripts/ButtonG.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonG : MonoBehaviour {
+
+	public GameObject fade;
+	private Animator anim;
+
+	void Start(){
+		anim = fade.GetComponent<Animator>();
+
+	}
+
+    public void OnClick()
+
+    {
+		
+		anim.SetTrigger("Fade");
+	   SceneManager.LoadScene("Scene02");
+
+
+    }
+}
